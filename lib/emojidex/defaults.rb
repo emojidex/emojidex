102fe52a26d33fbc7625a5c2c16e2e3031ca309b
影月 零 <zero@genshin.org>
2014-11-20 00:29:36 +0900

    fix romanization of はんこ

--- lib/emojidex/defaults.rb
+++ lib/emojidex/defaults.rb
@@ -4,7 +4,7 @@ module Emojidex
     def self.sizes
       { ldpi: 9, mdpi: 18, hdpi: 27, xhdpi: 36, px8: 8,
         px16: 16, px32: 32, px64: 64, px128: 128, px256: 256, px512: 512,
-        hannko: 90, seal: 320 }
+        hanko: 90, seal: 320 }
     end
 
     def self.formats
