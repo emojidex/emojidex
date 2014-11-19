8f576f41329ac2f72ddc4f7cdd81f09711887960
影月 零 <zero@genshin.org>
2014-11-19 23:20:04 +0900

    add new sizes

--- lib/emojidex/defaults.rb
+++ lib/emojidex/defaults.rb
@@ -3,7 +3,8 @@ module Emojidex
   class Defaults
     def self.sizes
       { ldpi: 9, mdpi: 18, hdpi: 27, xhdpi: 36, px8: 8,
-        px16: 16, px32: 32, px64: 64, px128: 128, px256: 256 }
+        px16: 16, px32: 32, px64: 64, px128: 128, px256: 256, px512: 512,
+        hannko: 90, seal: 320 }
     end
 
     def self.formats
