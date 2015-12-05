6add3c8c893d4056b98b26080fa6b65b2f7d4d72
影月 零 <zero@genshin.org>
2015-12-05 18:29:18 +0900

    add search spec mockup

---
+++ spec/emojidex/service/search_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+require 'emojidex/service/search'
+
+describe Emojidex::Service::Search do
+  describe '.term' do
+  end
+
+  describe '.search' do
+  end
+
+  describe '.starting' do
+  end
+
+  describe '.ending' do
+  end
+
+  describe '.tags' do
+  end
+
+  describe '.find' do
+  end
+
+  describe '.advanced' do
+  end
+end
