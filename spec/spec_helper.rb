bda25fb6e2b9e43ecc56eb011e80690605ad53ed
影月 零 <zero@genshin.org>
2015-12-07 17:19:35 +0900

    get rid of unneeded overrides

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,19 +5,6 @@ require 'emojidex/vectors'
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
-if ENV['TRAVIS'] || ENV['COVERAGE']
-  require 'simplecov'
-
-  if ENV['TRAVIS']
-    require 'coveralls'
-    SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-  end
-
-  SimpleCov.start do
-    add_filter '/spec/'
-  end
-end
-
 def sample_collection(name)
   e = Emojidex::Data::Collection.new
   e.load_local_collection(File.expand_path("../support/sample_collections/#{name}", __FILE__))
