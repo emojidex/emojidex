b9f54b4dc4fbb92e8aa3d0a927dfd3ffe6bc68e2
影月 零 <zero@genshin.org>
2016-05-16 23:20:59 +0900

    fix bug where cache index was corrupted on successive write

--- emojidex.gemspec
+++ emojidex.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'emojidex'
-  s.version     = '0.3.1'
+  s.version     = '0.3.2'
   s.license     = 'emojiOL'
   s.summary     = 'emojidex Ruby tools'
   s.description = 'emojidex emoji handling, search and lookup, listing and caching functionality' \
--- lib/emojidex/data/collection/cache.rb
+++ lib/emojidex/data/collection/cache.rb
@@ -70,8 +70,7 @@ module Emojidex
         idx = Emojidex::Data::Collection.new
         idx.load_local_collection(destination) if FileTest.exist? "#{destination}/emoji.json"
         idx.add_emoji @emoji.values
-        #File.open("#{destination}/emoji.json", 'w') { |f| f.write idx.emoji.values.to_json }
-        write_index(destination)
+        idx.write_index(destination)
       end
 
       # [over]writes a sanitized index to the specified destination.
